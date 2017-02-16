c343f80dc88932a9197715222ec9437366879161
Eric Shaw Jr <ericshaw.linux@gmail.com>
2017-02-16 11:18:38 -0500

    start code for processing command line arguments

--- src/main.c
+++ src/main.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
-#include "pfr_type.c"
+#include <string.h>
 
-int main()
+int pfr_cmd_type_define(int argc, const char **argv)
 {
+    return 0;
+}
+
+int pfr_cmd_type_undefine(int argc, const char **argv)
+{
+    return 0;
+}
+
+int pfr_cmd_detail_set(int argc, const char **argv)
+{
+    return 0;
+}
+
+int pfr_cmd_help(int argc, const char **argv)
+{
+    return 0;
+}
+
+struct cmd_struct {
+    const char *cmd;
+    int (*fn)(int argc, const char **argv);
+};
+
+static struct cmd_struct commands[] = {
+    {"def",    pfr_cmd_type_define},
+    {"undef",  pfr_cmd_type_undefine},
+    {"set",    pfr_cmd_detail_set},
+    {"help",   pfr_cmd_help}
+};
+
+#define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
+
+static struct cmd_struct *get_builtin(const char *s)
+{
+    unsigned int i;
+    for(i = 0; i < ARRAY_SIZE(commands); i++) {
+        struct cmd_struct *p = commands + i;
+        if (!strcmp(s, p->cmd))
+            return p;
+    }
+    return NULL;
+}
+
+void usage(int argc, const char **argv, struct cmd_struct *cmd)
+{
+    printf("usage: %s\n", argv[0]);
+}
+
+int main(int argc, const char **argv)
+{
+    struct cmd_struct *cmd = NULL;
+    
+    if (argc >= 2)
+        cmd = get_builtin(argv[1]);
+    
+    if (cmd == NULL || !cmd->fn(argc, argv))
+        usage(argc, argv, cmd);
+    
     return 0;
 }
